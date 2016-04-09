f7b1023502c84f17ec3235edf7ccde2f5092c5a8
Jianfu Zhang <jeff.zhang223@gmail.com>
2016-04-09 14:20:11 -0700

    add google map

--- SuperTopSecretProj/SuperTopSecretProj/AppDelegate.swift
+++ SuperTopSecretProj/SuperTopSecretProj/AppDelegate.swift
@@ -7,15 +7,17 @@
 //
 
 import UIKit
+import GoogleMaps
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
-
+    
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         // Override point for customization after application launch.
+        GMSServices.provideAPIKey("AIzaSyADGc6u-e_vnnWoPYC_MAbG_0EJEfqw7t4")
         return true
     }
 
@@ -43,4 +45,3 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
 }
-
--- SuperTopSecretProj/SuperTopSecretProj/ViewController.swift
+++ SuperTopSecretProj/SuperTopSecretProj/ViewController.swift
@@ -7,11 +7,24 @@
 //
 
 import UIKit
+import GoogleMaps
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        let camera = GMSCameraPosition.cameraWithLatitude(-33.86,
+            longitude: 151.20, zoom: 6)
+        let mapView = GMSMapView.mapWithFrame(CGRectZero, camera: camera)
+        mapView.myLocationEnabled = true
+        self.view = mapView
+        
+        let marker = GMSMarker()
+        marker.position = CLLocationCoordinate2DMake(-33.86, 151.20)
+        marker.title = "Sydney"
+        marker.snippet = "Australia"
+        marker.map = mapView
         // Do any additional setup after loading the view, typically from a nib.
     }
 
