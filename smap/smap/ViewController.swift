75502b4f03453db16b3f86a5408d082081e3d288
Marika Lee <marika.lee@rakuten.com>
2016-04-10 18:30:35 -0700

    Remove SuperTopSecretProj and added GMaps directions API polyline

--- smap/smap/ViewController.swift
+++ smap/smap/ViewController.swift
@@ -7,8 +7,9 @@
 //
 
 import UIKit
-import GoogleMaps
 import SwiftyJSON
+import AFNetworking
+import GoogleMaps
 
 class ViewController: UIViewController {
     var businesses: [Business]!
@@ -17,29 +18,108 @@ class ViewController: UIViewController {
         
         super.viewDidLoad()
         
-        Business.searchWithTerm("Thai", completion: { (businesses: [Business]!, error: NSError!) -> Void in
-            self.businesses = businesses
-            
-            for business in businesses {
-                print(business.name!)
-                print(business.address!)
-            }
-        })
+        let service = "https://maps.googleapis.com/maps/api/directions/json"
+        let originLat = "38.5"
+        let originLong = "106.2"
+        let destLat = "38.5"
+        let destLong = "106.3"
         
-        //        super.viewDidLoad()
+        let urlString = "\(service)?origin=\(originLat),\(originLong)&destination=\(destLat),\(destLong)&mode=driving&units=metric&sensor=true&key=AIzaSyC-LflNZIou4Lzdk8Wg_RM-MfvaWpqVdng"
+        
+
+        let directionsURL = NSURL(string: urlString)
+        
+        let request = NSMutableURLRequest(URL: directionsURL!)
+        
+        request.HTTPMethod = "GET"
+        request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+        request.addValue("application/json", forHTTPHeaderField: "Accept")
+        let operation = AFHTTPRequestOperation(request: request)
+        operation.responseSerializer = AFJSONResponseSerializer()
+        
+        operation.setCompletionBlockWithSuccess({ (operation: AFHTTPRequestOperation!, responseObject: AnyObject!) -> Void in
+            
+            if let result = responseObject as? NSDictionary {
+                if let routes = result["routes"] as? [NSDictionary] {
+                    if let lines = routes[0]["overview_polyline"] as? NSDictionary {
+                        if let points = lines["points"] as? String {
+                            let path = GMSPath(fromEncodedPath: points)
+                            let distance = GMSGeometryLength(path!)
+                            
+                            
+                            super.viewDidLoad()
+                            
+                            
+                            let camera = GMSCameraPosition.cameraWithLatitude(Double(originLat)!, longitude: Double(originLong)!, zoom: 15)
+                            
+                            let mapView = GMSMapView.mapWithFrame(CGRectZero, camera: camera)
+                            mapView.myLocationEnabled = true
+                            self.view = mapView
+                            
+                            let marker = GMSMarker()
+                            let marker2 = GMSMarker()
+                            
+                            marker.position = CLLocationCoordinate2DMake(Double(originLat)!, Double(originLong)!)
+                            marker.title = "Sydney1"
+                            marker.snippet = "Australia1"
+                            marker.map = mapView
+                            
+                            
+                            marker2.position = CLLocationCoordinate2DMake(Double(destLat)!, Double(destLong)!)
+                            marker2.title = "Sydney2"
+                            marker2.snippet = "Australia1"
+                            marker2.map = mapView
+                            
+                            
+                            let polyline: GMSPolyline? = nil
+                            if let p = path {
+                                let polyline = GMSPolyline(path: p)
+                                polyline.map = mapView
+                                
+                            }
+                            
+                            print("wow \(distance / 1000) KM")
+                            
+                        }
+                    }
+                }
+            }
+            }) { (operation: AFHTTPRequestOperation!, error: NSError!)  -> Void in
+                print("\(error)")
+        }
+        
+        operation.start()
+        
+        
+        
+        
         //
-        //        let camera = GMSCameraPosition.cameraWithLatitude(-33.86,
-        //            longitude: 151.20, zoom: 6)
-        //        let mapView = GMSMapView.mapWithFrame(CGRectZero, camera: camera)
-        //        mapView.myLocationEnabled = true
-        //        self.view = mapView
+        //        Business.searchWithTerm("Thai", completion: { (businesses: [Business]!, error: NSError!) -> Void in
+        //            self.businesses = businesses
         //
-        //        let marker = GMSMarker()
-        //        marker.position = CLLocationCoordinate2DMake(-33.86, 151.20)
-        //        marker.title = "Sydney"
-        //        marker.snippet = "Australia"
-        //        marker.map = mapView
-        // Do any additional setup after loading the view, typically from a nib.
+        //            for business in businesses {
+        //                print(business.name!)
+        //                print(business.address!)
+        //            }
+        //        })
+        
+        
+        //
+        //                super.viewDidLoad()
+        //
+        //                let camera = GMSCameraPosition.cameraWithLatitude(-33.86,
+        //                    longitude: 151.20, zoom: 6)
+        //                let mapView = GMSMapView.mapWithFrame(CGRectZero, camera: camera)
+        //                mapView.myLocationEnabled = true
+        //                self.view = mapView
+        //
+        //                let marker = GMSMarker()
+        //                marker.position = CLLocationCoordinate2DMake(-33.86, 151.20)
+        //                marker.title = "Sydney"
+        //                marker.snippet = "Australia"
+        //                marker.map = mapView
+        //
+        
     }
     
     override func didReceiveMemoryWarning() {
