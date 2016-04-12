f95e64209d64a8328e17ec8452cc6d283bf3d364
Marika Lee <marika.lee@rakuten.com>
2016-04-11 17:56:26 -0700

    Added user text box to output directions (origin and destination) on GMaps

--- smap/smap/ViewController.swift
+++ smap/smap/ViewController.swift
@@ -12,92 +12,22 @@ import AFNetworking
 import GoogleMaps
 
 class ViewController: UIViewController {
+    
     var businesses: [Business]!
+    var directions = GoogleDirectionsRoute()
     
     //Mark: properties
+
+    @IBOutlet var _originLat: UITextField!
+    @IBOutlet var _originLong: UITextField!
+    @IBOutlet var _destLat: UITextField!
+    @IBOutlet var _destLong: UITextField!
     
-    @IBOutlet weak var destinationTextField: UITextField!
-    @IBOutlet weak var destinationLabel: UILabel!
     
     override func viewDidLoad() {
         
         super.viewDidLoad()
-        //        let service = "https://maps.googleapis.com/maps/api/directions/json"
-        //        let originLat = "38.5"
-        //        let originLong = "106.2"
-        //        let destLat = "38.5"
-        //        let destLong = "106.3"
-        //
-        //        let urlString = "\(service)?origin=\(originLat),\(originLong)&destination=\(destLat),\(destLong)&mode=driving&units=metric&sensor=true&key=AIzaSyC-LflNZIou4Lzdk8Wg_RM-MfvaWpqVdng"
-        //
-        //
-        //        let directionsURL = NSURL(string: urlString)
-        //
-        //        let request = NSMutableURLRequest(URL: directionsURL!)
-        //
-        //        request.HTTPMethod = "GET"
-        //        request.addValue("application/json", forHTTPHeaderField: "Content-Type")
-        //        request.addValue("application/json", forHTTPHeaderField: "Accept")
-        //        let operation = AFHTTPRequestOperation(request: request)
-        //        operation.responseSerializer = AFJSONResponseSerializer()
-        //
-        //        operation.setCompletionBlockWithSuccess({ (operation: AFHTTPRequestOperation!, responseObject: AnyObject!) -> Void in
-        //
-        //
-        //            if let result = responseObject as? NSDictionary {
-        //                if let routes = result["routes"] as? [NSDictionary] {
-        //                    if let lines = routes[0]["overview_polyline"] as? NSDictionary {
-        //                        if let points = lines["points"] as? String {
-        //                            let path = GMSPath(fromEncodedPath: points)
-        //                            let distance = GMSGeometryLength(path!)
-        //
-        //
-        //                            super.viewDidLoad()
-        //
-        //
-        //                            let camera = GMSCameraPosition.cameraWithLatitude(Double(originLat)!, longitude: Double(originLong)!, zoom: 15)
-        //
-        //                            let mapView = GMSMapView.mapWithFrame(CGRectZero, camera: camera)
-        //                            mapView.myLocationEnabled = true
-        //                            self.view = mapView
-        //
-        //                            let marker = GMSMarker()
-        //                            let marker2 = GMSMarker()
-        //
-        //                            marker.position = CLLocationCoordinate2DMake(Double(originLat)!, Double(originLong)!)
-        //                            marker.title = "Sydney1"
-        //                            marker.snippet = "Australia1"
-        //                            marker.map = mapView
-        //
-        //
-        //                            marker2.position = CLLocationCoordinate2DMake(Double(destLat)!, Double(destLong)!)
-        //                            marker2.title = "Sydney2"
-        //                            marker2.snippet = "Australia1"
-        //                            marker2.map = mapView
-        //
-        //
-        //                            let polyline: GMSPolyline? = nil
-        //                            if let p = path {
-        //                                let polyline = GMSPolyline(path: p)
-        //                                polyline.map = mapView
-        //
-        //                            }
-        //
-        //                            print("wow \(distance / 1000) KM")
-        //
-        //                        }
-        //                    }
-        //                }
-        //            }
-        //            }) { (operation: AFHTTPRequestOperation!, error: NSError!)  -> Void in
-        //                print("\(error)")
-        //        }
-        //
-        //        operation.start()
-        //
         
-        
-        //
         //        Business.searchWithTerm("Thai", completion: { (businesses: [Business]!, error: NSError!) -> Void in
         //            self.businesses = businesses
         //
@@ -106,24 +36,20 @@ class ViewController: UIViewController {
         //                print(business.address!)
         //            }
         //        })
-        
-        
         //
-        //                super.viewDidLoad()
         //
-        //                let camera = GMSCameraPosition.cameraWithLatitude(-33.86,
-        //                    longitude: 151.20, zoom: 6)
-        //                let mapView = GMSMapView.mapWithFrame(CGRectZero, camera: camera)
-        //                mapView.myLocationEnabled = true
-        //                self.view = mapView
+        //        let camera = GMSCameraPosition.cameraWithLatitude(-33.86,
+        //            longitude: 151.20, zoom: 6)
+        //        let mapView = GMSMapView.mapWithFrame(CGRectZero, camera: camera)
+        //        mapView.myLocationEnabled = true
+        //        self.view = mapView
         //
-        //                let marker = GMSMarker()
-        //                marker.position = CLLocationCoordinate2DMake(-33.86, 151.20)
-        //                marker.title = "Sydney"
-        //                marker.snippet = "Australia"
-        //                marker.map = mapView
+        //        let marker = GMSMarker()
+        //        marker.position = CLLocationCoordinate2DMake(-33.86, 151.20)
+        //        marker.title = "Sydney"
+        //        marker.snippet = "Australia"
+        //        marker.map = mapView
         //
-        
         
     }
     
@@ -132,17 +58,16 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-    //Mark: Actions
-    
     @IBAction func setDefaultDestination(sender: UIButton) {
         
-        var directions = GoogleDirectionsRoute()
         
         let service = "https://maps.googleapis.com/maps/api/directions/json"
-        let originLat = "38.5"
-        let originLong = "106.2"
-        let destLat = "38.5"
-        let destLong = "106.3"
+
+        let originLat = _originLat.text!
+        let originLong = _originLong.text!
+        
+        let destLat = _destLat.text!
+        let destLong = _destLong.text!
         
         let urlString = "\(service)?origin=\(originLat),\(originLong)&destination=\(destLat),\(destLong)&mode=driving&units=metric&sensor=true&key=AIzaSyC-LflNZIou4Lzdk8Wg_RM-MfvaWpqVdng"
         
@@ -159,28 +84,28 @@ class ViewController: UIViewController {
         
         operation.setCompletionBlockWithSuccess({ (operation: AFHTTPRequestOperation!, responseObject: AnyObject!) -> Void in
             
-            
             if let result = responseObject as? NSDictionary {
                 if let routes = result["routes"] as? [NSDictionary] {
                     if let lines = routes[0]["overview_polyline"] as? NSDictionary {
                         if let points = lines["points"] as? String {
                             let path = GMSPath(fromEncodedPath: points)
                             let distance = GMSGeometryLength(path!)
-               
+                            
                             let camera = GMSCameraPosition.cameraWithLatitude(Double(originLat)!, longitude: Double(originLong)!, zoom: 15)
                             
                             let mapView = GMSMapView.mapWithFrame(CGRectZero, camera: camera)
                             mapView.myLocationEnabled = true
                             self.view = mapView
                             
-                            directions.drawOnMap(mapView, path: path)
-                            directions.drawOriginMarkerOnMap(mapView, path: path)
-                            directions.drawDestinationMarkerOnMap(mapView, path: path)
+                            self.directions.drawOnMap(mapView, path: path)
+                            self.directions.drawOriginMarkerOnMap(mapView, path: path)
+                            self.directions.drawDestinationMarkerOnMap(mapView, path: path)
                             
                             print("wow \(distance / 1000) KM")
                             
                         }
                     }
+                    
                 }
             }
             }) { (operation: AFHTTPRequestOperation!, error: NSError!)  -> Void in
@@ -191,5 +116,4 @@ class ViewController: UIViewController {
         
         
     }
-    
-}
+   }
