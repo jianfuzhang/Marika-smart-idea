5779d8b82496f207059b4993997f66a821d37d42
Marika Lee <marika.lee@rakuten.com>
2016-04-12 17:58:25 -0700

    Refactor so all JSON parsing uses Swifty and have http call from API (origin, dest)

--- smap/smap/ViewController.swift
+++ smap/smap/ViewController.swift
@@ -13,25 +13,15 @@ import GoogleMaps
 
 class ViewController: UIViewController {
     
-    var businesses: [Business]!
     var directions = GoogleDirectionsRoute()
     
     //Mark: properties
-    
-    
-    @IBOutlet var _originLat: UITextField!
-    @IBOutlet var _originLong: UITextField!
-    @IBOutlet var _destLat: UITextField!
-    @IBOutlet var _destLong: UITextField!
-    
     @IBOutlet var _originAddr: UITextField!
     @IBOutlet var _destAddr: UITextField!
     
     
     override func viewDidLoad() {
-        
-        super.viewDidLoad()
-        
+        super.viewDidLoad()       
     }
     
     override func didReceiveMemoryWarning() {
@@ -46,9 +36,7 @@ class ViewController: UIViewController {
         let originAddr = _originAddr.text!
         let destAddr = _destAddr.text!
         
-        let urlString = "https://maps.googleapis.com/maps/api/directions/json?origin=800_Concar+Ave+San+Mateo,+CA&destination=1622+Palm+Ave+San+Mateo,+CA&key=AIzaSyC-LflNZIou4Lzdk8Wg_RM-MfvaWpqVdng"
-        
-        //      let urlString = "\(service)?origin=\(originAddr)&destination=\(destAddr)&mode=driving&units=metric&sensor=true&key=AIzaSyC-LflNZIou4Lzdk8Wg_RM-MfvaWpqVdng"
+        let urlString = ("\(service)?origin=\(originAddr)&destination=\(destAddr)&mode=driving&units=metric&sensor=true&key=AIzaSyC-LflNZIou4Lzdk8Wg_RM-MfvaWpqVdng").stringByReplacingOccurrencesOfString(" ", withString: "+")
         
         let directionsURL = NSURL(string: urlString)
         
@@ -65,32 +53,23 @@ class ViewController: UIViewController {
             if let result = responseObject as? NSDictionary {
                 if let routes = result["routes"] as? [NSDictionary] {
                     
-                    let origin = routes[0]["bounds"]!["northeast"] as? NSDictionary
+                    let routesJson = JSON(routes)
                     
-                    let originLat = origin!["lat"] as? Double
-                    let originLong = origin!["lng"] as? Double
+                    let originLat = routesJson[0]["bounds"]["northeast"]["lat"].doubleValue
+                    let originLong = routesJson[0]["bounds"]["northeast"]["lng"].doubleValue
+                    let points = routesJson[0]["overview_polyline"]["points"].stringValue
                     
+                    let path = GMSPath(fromEncodedPath: points)
                     
-                    if let lines = routes[0]["overview_polyline"] as? NSDictionary {
-                        if let points = lines["points"] as? String {
-                            
-                            let path = GMSPath(fromEncodedPath: points)
-                            let distance = GMSGeometryLength(path!)
-                            
-                            let camera = GMSCameraPosition.cameraWithLatitude(originLat!, longitude: originLong!, zoom: 15)
-                            
-                            let mapView = GMSMapView.mapWithFrame(CGRectZero, camera: camera)
-                            mapView.myLocationEnabled = true
-                            self.view = mapView
-                            
-                            self.directions.drawOnMap(mapView, path: path)
-                            self.directions.drawOriginMarkerOnMap(mapView, path: path)
-                            self.directions.drawDestinationMarkerOnMap(mapView, path: path)
-                            
-                            print("wow \(distance / 1000) KM")
-                            
-                        }
-                    }
+                    let camera = GMSCameraPosition.cameraWithLatitude(originLat, longitude:originLong, zoom: 15)
+                    
+                    let mapView = GMSMapView.mapWithFrame(CGRectZero, camera: camera)
+                    mapView.myLocationEnabled = true
+                    self.view = mapView
+                    
+                    self.directions.drawOnMap(mapView, path: path)
+                    self.directions.drawOriginMarkerOnMap(mapView, path: path)
+                    self.directions.drawDestinationMarkerOnMap(mapView, path: path)
                     
                 }
             }
@@ -99,7 +78,6 @@ class ViewController: UIViewController {
         }
         
         operation.start()
-        
         
     }
 }
